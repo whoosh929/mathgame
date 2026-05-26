d885faf70186663291cf284bbcf502ef87895c55
Kevin <kevin.shyu@gmail.com>
2026-05-25 18:51:10 -0700

    added division and new pics

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,30 +2,73 @@ import Piece from "./Piece.tsx";
 import type { boardProps } from "../constants/interface.tsx";
 import "../styles/boardStyle.css";
 import { getRandomNumber } from "../utils/randomizer.ts";
+import { useState, useEffect } from "react";
 
 function Board({ numbers, op }: boardProps) {
 	//todo: make row and length scaleable, make operator carry over as setting
 	console.log("Board entry - op:" + op);
+
+	//these gen functions are a shittier way to allow for clean divisor numbers
+	function genFirstNumber({ numbers, op }: boardProps): number {
+		const num = getRandomNumber(numbers.firstNumMin, numbers.firstNumMax);
+		return num;
+	}
+
+	function genSecondNumber({ numbers, op }: boardProps, firstNum?: number): number {
+		let num = 0;
+
+		if (op == "divide") {
+			num = getRandomNumber(numbers.secondNumMin, numbers.secondNumMax, firstNum);
+		} else {
+			num = getRandomNumber(numbers.secondNumMin, numbers.secondNumMax);
+		}
+		return num;
+	}
+	// Generate numbers for each piece individually
+	const generateNumberPair = () => {
+		const first = genFirstNumber({ numbers, op });
+		const second = genSecondNumber({ numbers, op }, first);
+		return { first, second };
+	};
 
 	return (
 		<table id="tableBoard">
 			<tbody>
 				{Array.from({ length: 3 }).map((_, row) => (
 					<tr key={row}>
-						{Array.from({ length: 4 }).map((_, col) => (
-							<td key={`${row}-${col}`}>
-								<Piece
-									firstNum={getRandomNumber(numbers.firstNumMin, numbers.firstNumMax)}
-									secondNum={getRandomNumber(numbers.secondNumMin, numbers.secondNumMax)}
-									operator={op}
-								/>
-							</td>
-						))}
+						{Array.from({ length: 4 }).map((_, col) => {
+							const { first, second } = generateNumberPair();
+							return (
+								<td key={`${row}-${col}`}>
+									<Piece firstNum={first} secondNum={second} operator={op} />
+								</td>
+							);
+						})}
 					</tr>
 				))}
 			</tbody>
 		</table>
 	);
 }
+
+/*
+	return (
+		<table id="tableBoard">
+			<tbody>
+				{Array.from({ length: 3 }).map((_, row) => (
+					<tr key={row}>
+						{Array.from({ length: 4 }).map((_, col) => (
+						
+							return(
+							<td key={`${row}-${col}`}>
+								<Piece firstNum={first} secondNum={second} operator={op} />
+							</td>;
+						))}
+					</tr>
+				))}
+			</tbody>
+		</table>
+	);
+}*/
 
 export default Board;
--- src/utils/randomizer.ts
+++ src/utils/randomizer.ts
@@ -1,20 +1,42 @@
-function getRandomNumber(min: number, max: number): number {
-	let num = Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomDivsor(number: number): number {
+	//get all divsors of the number
+	const divisors: number[] = [];
 
+	for (let i = 1; i <= number; i++) {
+		if (number % i === 0) {
+			divisors.push(i);
+		}
+	}
+
+	//return a random divisor
+	console.log("divisors: " + divisors);
+	const randomIndex = Math.floor(Math.random() * divisors.length);
+	return divisors[randomIndex];
+}
+function getRandomNumber(min: number, max: number, divNum?: number): number {
+	//this function allows to use a number that is divisible (ie mod == 0)
+	//to do this, feed in optional parameter divNum as the 'other' number
+	let num = 0;
+	if (divNum == undefined) {
+		//if not division
+		num = Math.floor(Math.random() * (max - min + 1)) + min;
+	} else {
+		//if division
+		do {
+			num = getRandomDivsor(divNum);
+		} while (num < min || num > max);
+	}
 	return num;
 }
 
 //todo: getRandomImage still doesn't work
 function getRandomImage(): string {
-	const images = [
-		"C:\\Users\\kevin\\react projects\\mathGame\\src\\assets\\images\\bathing_marshmellow.png",
-		"C:\\Users\\kevin\\react projects\\mathGame\\src\\assets\\images\\cooking_pot.png",
-		"C:\\Users\\kevin\\react projects\\mathGame\\src\\assets\\images\\crocodile_jungle.png",
-		"C:\\Users\\kevin\\react projects\\mathGame\\src\\assets\\images\\happy_clam.png",
-		"C:\\Users\\kevin\\react projects\\mathGame\\src\\assets\\images\\raining_octopus.jpg",
-		"C:\\Users\\kevin\\react projects\\mathGame\\src\\assets\\images\\robot_crab.png",
-		"C:\\Users\\kevin\\react projects\\mathGame\\src\\assets\\images\\space_alien.png",
-	];
+	// Dynamically import all images in the directory
+	const imageModules = import.meta.glob("../assets/images/*.{png,jpg,jpeg,svg}", {
+		eager: true,
+		import: "default",
+	});
+	const images = Object.values(imageModules) as string[];
 
 	const randomImage = images[Math.floor(Math.random() * images.length)];
 	console.log("randomImage: " + randomImage);
